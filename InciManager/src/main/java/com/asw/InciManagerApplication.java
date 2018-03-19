739e5b1ec0e90036324c67914b0d485b811091a0
joaquin_ <uo250687@uniovi.es>
2018-03-19 23:02:23 +0100

    empezando

--- InciManager/src/main/java/com/asw/InciManagerApplication.java
+++ InciManager/src/main/java/com/asw/InciManagerApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class InciManagerApplication {
 
+	// commit
+
 	public static void main(String[] args) {
 		SpringApplication.run(InciManagerApplication.class, args);
 	}
