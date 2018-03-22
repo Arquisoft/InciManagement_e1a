35bc509da48e74c2d1976aa8448e27e7bc5bc5a1
paula <tinap@192.168.56.1>
2018-03-22 11:47:24 +0100

    rama con mis cambios

--- src/main/java/com/asw/InciManagerApplication.java
+++ src/main/java/com/asw/InciManagerApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class InciManagerApplication {
 
-	// commit
 
 	public static void main(String[] args) {
 		SpringApplication.run(InciManagerApplication.class, args);
