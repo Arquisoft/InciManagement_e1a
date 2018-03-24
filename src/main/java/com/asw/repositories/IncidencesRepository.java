7eaa394228780e9d0ec8b5644b5e9f2efbe36c8f
joaquin_ <uo250687@uniovi.es>
2018-03-24 22:10:50 +0100

    cambio de rama

--- src/main/java/com/asw/repositories/IncidencesRepository.java
+++ src/main/java/com/asw/repositories/IncidencesRepository.java
@@ -1,9 +1,13 @@
 package com.asw.repositories;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.asw.entities.Incidence;
 
 public interface IncidencesRepository extends CrudRepository<Incidence, Long> {
+
+	List<Incidence> findByNombreAgente(String userName);
 
 }
