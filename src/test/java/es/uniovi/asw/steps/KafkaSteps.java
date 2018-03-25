df423db1229706c6df072b19a027d65fa9b76f83
UO250678 <UO250678@uniovi.es>
2018-03-25 16:42:16 +0200

    Corrección de ciertos errores cucumber

--- src/test/java/es/uniovi/asw/steps/KafkaSteps.java
+++ src/test/java/es/uniovi/asw/steps/KafkaSteps.java
@@ -6,8 +6,9 @@ import cucumber.api.java.en.When;
 
 import com.asw.InciManagerApplication;
 import com.asw.controllers.Main;
-//import es.uniovi.asw.listeners.MessageListener;
+import com.asw.listeners.MessageListener;
 import com.asw.Kafka.producer.KafkaProducer;
+
 
 
 
@@ -23,7 +24,7 @@ import org.springframework.boot.test.SpringApplicationContextLoader;
 //import org.springframework.boot.test.SpringApplicationConfiguration;
 //import org.springframework.boot.test.SpringApplicationContextLoader;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.listener.MessageListener;
+//import org.springframework.kafka.listener.MessageListener;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -46,21 +47,21 @@ public class KafkaSteps {
   @Autowired
   private MessageListener messageListener;
 
-  private Observable<String> observable;
+  private io.reactivex.Observable<String> observable;
   
   private static final Logger LOG = LoggerFactory.getLogger(Main.class);
 
   @When("^the producer sends the message \"([^\"]*)\" to the topic \"([^\"]*)\"$")
   public void the_producer_sends_the_message_to_the_topic(String message, String topic) throws Throwable {
-   // observable = messageListener.getObservable();
+    observable = messageListener.getObservable();
     kafkaProducer.send(topic, message);
   }
 
   @Then("^the consumer receives the message \"([^\"]*)\" from the topic \"([^\"]*)\"$")
   public void the_consumer_receives_the_message_from_the_topic(String message, String topic) throws Throwable {
-   // String receivedMessage = observable.blockingFirst();
-   // System.out.println("Message received: " + receivedMessage);
-   // assertTrue(receivedMessage.equals(message));
+    String receivedMessage = observable.blockingFirst();
+    System.out.println("Message received: " + receivedMessage);
+    assertTrue(receivedMessage.equals(message));
   }
 
 }
