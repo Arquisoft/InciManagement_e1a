cf41b96fca4384186d8478b43cba4d2deefb7489
FernandoDelBarrio <fernicolombres@gmail.com>
2018-05-06 20:13:12 +0200

    cambio IP

--- src/main/java/com/asw/services/AgentsService.java
+++ src/main/java/com/asw/services/AgentsService.java
@@ -20,6 +20,7 @@ public class AgentsService {
 	public boolean checkAgent(String agentName, String password, String kind) {
 		RestTemplate template = new RestTemplate();
 		try {
+			
 			template.postForEntity("http://54.218.124.96:8090/agent", new PeticionInfoREST(agentName, password, kind),
 					String.class);
 			return true;
@@ -30,7 +31,7 @@ public class AgentsService {
 
 	public String getLocation(Incidence incidence) {
 		RestTemplate template = new RestTemplate();
-		ResponseEntity<String> response = template.postForEntity("http://18.236.186.26:8090/agent",
+		ResponseEntity<String> response = template.postForEntity("http://54.218.124.96:8090/agent",
 				new PeticionInfoREST(incidence.getNombreAgente(), incidence.getPasswordAgente(),
 						incidence.getTipoAgente()),
 				String.class);
